e75031e3dad3ea7378e2dcbb2b4517a8905536ad
USER <USER@DESKTOP-Q78I71E>
2023-08-22 15:43:27 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace OOP___CLASSES
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Chef chef = new Chef();
+            chef.name = "The cook";
+            chef.meal = "pastrami rice";
+
+
+        }
+
+        class Chef {
+         public   string name { get; set; }
+          public   string meal { get; set; }
+
+
+           
+
+
+            void cooking()
+            {
+                Console.WriteLine("This is the real chef cooking different delecases");
+            }
+
+
+            class italianchef:Chef
+                {
+                virtual void  specailDishes() 
+                {
+                    Console.WriteLine("this is another italian  chef agent cooking pastami rice");
+
+                }
+
+
+                }
+        }
+
+    }
+
+}
